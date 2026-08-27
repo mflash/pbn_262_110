822a1fbd869f92c510a90596ec2ca3d7340afee5
Marcelo Cohen <marcelo.cohen@pucrs.br>
2026-08-27 11:11:01 -0300

    Exerc. 2 de ponteiros: copia um vetor sobre outro

---
+++ 03_Ponteiros/exerc3_invertevetor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void inverteVetor(int v[], int tam);
+
+int main() {
+  int vet[6] = {10, 20, 30, 40, 50, 60};
+  inverteVetor(vet, 6);
+  // Escrever na tela o vetor invertido
+}
+
+void inverteVetor(int v[], int tam) {
+  // terminar!
+}
